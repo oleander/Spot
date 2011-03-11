4c4a696102fa294a90112189e2a82418a6e8d399
Linus Oleander <linus@oleander.nu>
2011-03-12 00:37:10 +0100

    Adding some comments to the base class

--- lib/spotify/base.rb
+++ lib/spotify/base.rb
@@ -5,18 +5,25 @@ module SpotifyContainer
     attr_reader :popularity, :name
     
     def initialize(args)
-      args.keys.each { |name| instance_variable_set "@" + name.to_s.gsub(/[^a-z]/i, ''), args[name]}
+      args.keys.each { |name| instance_variable_set "@" + name.to_s.gsub(/[^a-z]/i, ''), args[name] }
     end
     
     # Is the object it self valid?
     def valid?
       not_implemented
     end
-
+    
+    # Is the object available in {territory}?
+    # Where {territory} can be any string representation of the ISO 3166-1 alpha-2 table
+    # Read more about it here: http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
     def available?(territory = nil)
       not_implemented
     end
     
+    # Returns a URL for the object
+    # {type} can contain the value "spotify" or "href"
+    # href => http://open.spotify.com/track/5DhDGwNXRPHsMApbtVKvFb
+    # spotify => spotify:track:5DhDGwNXRPHsMApbtVKvFb
     def href(type = "spotify")
       send("href_#{type}")
     end
