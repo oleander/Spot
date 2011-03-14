73548c91cb8e775b45a815acbbc31849123c3fb6
Linus Oleander <linus@oleander.nu>
2011-03-14 03:21:26 +0100

    Moving helper methods into spec_helper. Is this the right place?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,31 @@ require "spotify"
 RSpec.configure do |config|
   config.mock_with :rspec
 end
+
+def mock_media(ret)
+  song = mock(Object.new)
+  song.should_receive(:valid?).any_number_of_times.and_return(ret)
+  song
+end
+
+def validate_artists(artists)
+  artists.each do |artist|
+    artist["name"].should_not be_empty
+    artist["href"].to_s.should match(/^spotify\:artist\:[a-zA-Z0-9]+|.{0}$/) # Can be blank
+  end
+end
+
+def set_up(times = 100, ret = true, klass = SpotifyContainer::Song)
+  klass.should_receive(:new).exactly(times).times.and_return(mock_media(ret))
+end
+
+def generate_url(type, search, page = 1)
+  "http://ws.spotify.com/search/1/#{type}.json?q=#{URI.escape(search)}&page=#{page}"
+end
+
+def stubs(type, search, page = 1)
+  url = generate_url(type, search, page)
+  stub_request(:get, url).
+    to_return(:body => File.read("spec/fixtures/#{type}.json"), :status => 200)
+  url
+end
--- spec/spotify_spec.rb
+++ spec/spotify_spec.rb
@@ -265,32 +265,4 @@ describe Spotify do
   
   it "should raise an error if the JSON method raises one"
   it "should do the same as above if the scrape raise an error"
-  
-  def mock_media(ret)
-    song = mock(Object.new)
-    song.should_receive(:valid?).any_number_of_times.and_return(ret)
-    song
-  end
-  
-  def validate_artists(artists)
-    artists.each do |artist|
-      artist["name"].should_not be_empty
-      artist["href"].to_s.should match(/^spotify\:artist\:[a-zA-Z0-9]+|.{0}$/) # Can be blank
-    end
-  end
-  
-  def set_up(times = 100, ret = true, klass = SpotifyContainer::Song)
-    klass.should_receive(:new).exactly(times).times.and_return(mock_media(ret))
-  end
-  
-  def generate_url(type, search, page = 1)
-    "http://ws.spotify.com/search/1/#{type}.json?q=#{URI.escape(search)}&page=#{page}"
-  end
-  
-  def stubs(type, search, page = 1)
-    url = generate_url(type, search, page)
-    stub_request(:get, url).
-      to_return(:body => File.read("spec/fixtures/#{type}.json"), :status => 200)
-    url
-  end
 end
