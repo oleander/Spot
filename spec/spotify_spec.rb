7a09623d49e5ce9d59911d94a570e2c69d1cb182
Linus Oleander <linus@oleander.nu>
2011-04-22 17:44:39 +0200

    Indenting code correctly

--- spec/spotify_spec.rb
+++ spec/spotify_spec.rb
@@ -327,22 +327,24 @@ describe Spotify do
     
     it "should have a working exclude? method" do
       {
-        "tribute" => true, 
-        "random" => false, 
-        "cover random" => true, 
-        "live" => true,
-        "club mix random" => true, 
-        "club random mix" => false,
-        "LIVE" => true,
-        "karaoKE" => true,
-        "instrumental" => true,
+        "tribute"                          => true,
+        "cover random"                     => true, 
+        "live"                             => true,
+        "club mix random"                  => true,
+        "LIVE"                             => true,
+        "karaoKE"                          => true,
+        "instrumental"                     => true,
         "Karaoke - Won't Get Fooled Again" => true,
-        "club version" => true,
-        "instrumentals" => true,
-        "demo" => true,
-        "made famous by" => true,
-        "remixes" => true,
-        "ringtone" => true
+        "club version"                     => true,
+        "instrumentals"                    => true,
+        "demo"                             => true,
+        "made famous by"                   => true,
+        "remixes"                          => true,
+        "ringtone"                         => true,
+        "ringtones"                        => true,
+        "riingtonerandom"                  => false,
+        "club random mix"                  => false,
+        "random"                           => false
       }.each do |comp, outcome|
         @spotify.exclude?(comp).should eq(outcome)
       end
