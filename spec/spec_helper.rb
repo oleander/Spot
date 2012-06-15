8a24b50cffb0fab386da91c935943417f1ef0980
Linus Oleander <linus@oleander.nu>
2012-06-15 17:37:12 +0200

    Use VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,20 @@
 require "rspec"
-require "webmock/rspec"
 require "spot"
+require "vcr"
+require "webmock/rspec"
 
 RSpec.configure do |config|
   config.mock_with :rspec
+  config.extend VCR::RSpec::Macros
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.hook_into :webmock
+  c.default_cassette_options = {
+    record: :all
+  }
+  c.allow_http_connections_when_no_cassette = false
 end
 
 def mock_media(ret)
@@ -28,8 +39,8 @@ def generate_url(type, search, page = 1)
 end
 
 def stubs(type, search, page = 1)
-  url = generate_url(type, search, page)
-  stub_request(:get, url).
-    to_return(:body => File.read("spec/fixtures/#{type}.json"), :status => 200)
-  url
+  # url = generate_url(type, search, page)
+  # stub_request(:get, url).
+  #   to_return(:body => File.read("spec/fixtures/#{type}.json"), :status => 200)
+  # url
 end
--- spot.gemspec
+++ spot.gemspec
@@ -25,5 +25,6 @@ Gem::Specification.new do |s|
   s.add_dependency("rchardet19")
   
   s.add_development_dependency("rspec")
-  s.add_development_dependency("webmock")
+  s.add_development_dependency("vcr")
+  s.add_development_dependency("webmock", "~> 1.8.0")
 end
